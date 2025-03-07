6b8e3b3349368e22a656c546f3f8240b40035792
Osmane <osmane.sgb@gmail.com>
2025-03-07 02:02:49 +0100

    ex02 and add header to includes + remove useless colors.hpp

---
+++ ex02/src/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obouayed <obouayed@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/07 01:58:37 by obouayed          #+#    #+#             */
+/*   Updated: 2025/03/07 02:00:33 by obouayed         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string string = "HI THIS IS BRAIN";
+    std::string* stringPTR = &string;
+    std::string& stringREF = string;
+
+    // Print memory addresses
+    std::cout << "Memory address of the string variable: " << &string << std::endl;
+    std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
+    std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
+
+    // Print values
+    std::cout << "Value of the string variable: " << string << std::endl;
+    std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+    std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+
+    return 0;
+}
+
